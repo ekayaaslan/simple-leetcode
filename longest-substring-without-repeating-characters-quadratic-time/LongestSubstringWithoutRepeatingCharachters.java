890ffe165940c9471509f21b7447bed78d3441ed
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-04 17:22:53 +0000

    Create LongestSubstringWithoutRepeatingCharachters.java

---
+++ longest-substring-without-repeating-characters-quadratic-time/LongestSubstringWithoutRepeatingCharachters.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters
+// LC3. Longest Substring Without Repeating Characters
+// Quadratic time, constant extra-space.
+// Concepts: Linear Search, Direct addressing.
+class LongestSubstringWithoutRepeatingCharachters {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        boolean[] seen = new boolean[128];
+        int longest = 0;
+        for (int i=0; i<n; i++) {
+            for (int d=0; d<128; d++) { seen[d] = false; }
+            for (int j=i; j<n; j++) {
+                char ch = s.charAt(j);
+                if (seen[ch]) { break; }
+                seen[ch] = true;
+                longest = Math.max(longest, j-i+1);
+            }
+        }
+        return longest;
+    }
+}
