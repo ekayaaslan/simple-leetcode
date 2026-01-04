773c1480810e6b854fe525685984b3fe00bb5d0e
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-04 13:30:21 +0000

    Create ContainerWithMostWater.java

---
+++ container-with-most-water-quadratic-time/ContainerWithMostWater.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/container-with-most-water/
+// LC11. Container With Most Water
+// Time/space: Quadratic time, constant extra-space.
+// Approach: Brute force.
+// Concepts: Linear Search.
+class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int max = 0;
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                int area = Math.min(height[i], height[j]) * (j-i);
+                max = Math.max(max, area);
+            }
+        }
+        return max;
+    }
+}
