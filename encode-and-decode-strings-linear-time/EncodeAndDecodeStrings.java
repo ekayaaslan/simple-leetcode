b5824655bce7cdf425c076a3019aa7bec1e2d822
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-04 18:39:48 +0000

    Update EncodeAndDecodeStrings.java

--- encode-and-decode-strings-linear-time/EncodeAndDecodeStrings.java
+++ encode-and-decode-strings-linear-time/EncodeAndDecodeStrings.java
@@ -1,7 +1,6 @@
 // https://leetcode.com/problems/encode-and-decode-strings
 // LC271. Encode And Decode Strings
-// Time/space: Linear time.
-// Concepts: Encoding.
+// Linear time, linear extra-space.
 public class EncodeAndDecodeStrings {
 
     // Encodes a list of strings to a single string.
