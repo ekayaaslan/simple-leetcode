cf47494d59546277c0e0c8bea498956839a3610c
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-04 17:15:57 +0000

    Create TwoSum.java

---
+++ two-sum-linearish-time-v2/TwoSum.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/two-sum
+// LC1. Two Sum
+// Linear-ish time, linear-ish extra-space.
+// Concepts: Linear Search, Hashing.
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numsSoFar = new HashMap();
+        int n = nums.length;
+        for(int i=0; i<n; i++) {
+            int complement = target-nums[i];
+            if (numsSoFar.containsKey(complement)) {
+                return new int[] {numsSoFar.get(complement), i};
+            }
+            numsSoFar.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
