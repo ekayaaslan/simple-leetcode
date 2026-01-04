a514e6a883c45da1054d0380668e06d880dc8310
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-04 18:41:02 +0000

    Update 3Sum.java

--- 3sum-cubic-time/3Sum.java
+++ 3sum-cubic-time/3Sum.java
@@ -1,7 +1,6 @@
 // https://leetcode.com/problems/3sum
 // LC15. 3Sum
-// Time/space: Cubic time, constant extra-space.
-// Concepts: Linear search, Sorting.
+// Cubic time, constant extra-space.
 class 3Sum {
     class Triplet implements Comparable<Triplet> {
         int a, b, c;
