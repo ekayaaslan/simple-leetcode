cb5659fd146275c39a16dd17368b6cf3b3c20ee8
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-04 00:15:03 +0000

    Create ProductOfArrayExceptSelf.java

---
+++ LC238. Product Of Array Except Self. Quadratic-time./ProductOfArrayExceptSelf.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/product-of-array-except-self
+// LC238. ProductOfArrayExceptSelf
+// Time/space: Quadratic time, constant extra-space.
+// Solution kind: Baseline.
+// Approach: Brute force.
+class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i=0; i<n; i++) {
+            int product = 1;
+            for (int j=0; j<n; j++) {
+                if (i == j) { continue; }
+                product *= nums[j];
+            }
+            ans[i] = product;
+        }
+        return ans;
+    }
+}
