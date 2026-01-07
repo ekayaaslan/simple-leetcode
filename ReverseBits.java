21b1d41baa5b90812e06c579154a5338a7be4f4b
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-07 22:43:02 +0000

    Create ReverseBits.java

---
+++ ReverseBits.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/reverse-bits
+// LC190. Reverse Bits
+// Constant time, constant extra-space.
+// Digits, Binary.
+class ReverseBits {
+    public int reverseBits(int n) {
+        int num = n;
+        int reverse = 0;
+        for (int i=0; i<32; i++) {
+            int bit = num%2;
+            reverse = reverse*2 + bit;
+            num = num/2;
+        }
+        return reverse;
+    }
+}
