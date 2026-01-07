14a0db6c4a47f661becd27b47673c29f54804a48
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-07 22:46:08 +0000

    Create MissingNumber.java

---
+++ MissingNumber.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/missing-number
+// LC268. Missing Number
+// Linear time, linear extra-space.
+// Brute force, Direct addressing.
+class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        boolean[] seen = new boolean[n+1];
+        for (int i=0; i<n; i++) {
+            seen[nums[i]] = true;
+        }
+        for (int i=0; i<=n; i++) {
+            if (!seen[i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
