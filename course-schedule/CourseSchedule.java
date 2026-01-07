0ce772e014c41f347ac3b45245c27705e565dba1
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-07 11:27:35 +0000

    Create CourseSchedule.java

---
+++ course-schedule/CourseSchedule.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/course-schedule
+// LC207. Course Schedule
+class CourseSchedule {
+    boolean canFinishAny(int n, int[][] prerequisites, boolean[] finish) {
+        int m = prerequisites.length;
+        boolean[] hasDependencies = new boolean[n];
+        for (int j=0; j<m; j++) {
+            int a = prerequisites[j][0];
+            int b = prerequisites[j][1];
+            if (!finish[a]) { hasDependencies[b] = true; }
+        }
+        for (int i=0; i<n; i++) {
+            if (finish[i]) { continue; }
+            if (!hasDependencies[i]) {
+                finish[i] = true;
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n = numCourses;
+        boolean[] finish = new boolean[n];
+        for (int i=0; i<n; i++) {
+            if (!canFinishAny(n, prerequisites, finish)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
