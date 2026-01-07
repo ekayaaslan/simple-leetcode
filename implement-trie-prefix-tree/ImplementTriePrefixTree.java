51f14d07b9a4f6b995983b44dc9cc1813885f4d6
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-07 09:18:51 +0000

    Create ImplementTriePrefixTree.java

---
+++ implement-trie-prefix-tree/ImplementTriePrefixTree.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/implement-trie-prefix-tree
+// LC208. Implement Trie Prefix Tree
+class ImplementTriePrefixTree {
+
+    List<String> words;
+    public Trie() {
+        words = new ArrayList();
+    }
+    
+    public void insert(String word) {
+        words.add(word);
+    }
+    
+    public boolean search(String word) {
+        for (String w : words) {
+            if (w.equals(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean startsWith(String prefix) {
+        for (String word : words) {
+            if (word.startsWith(prefix)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
