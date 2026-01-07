99557350c614eb7751459ac60a0eb78461f0978f
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-07 09:23:19 +0000

    Create DesignAddAndSearchWordsDataStructure.java

---
+++ design-add-and-search-words-data-structure/DesignAddAndSearchWordsDataStructure.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/design-add-and-search-words-data-structure
+// LC211. Design Add and Search Words Data Structure
+class DesignAddAndSearchWordsDataStructure {
+
+    List<String> words;
+
+    public WordDictionary() {
+        words = new ArrayList();
+    }
+    
+    public void addWord(String word) {
+        words.add(word);
+    }
+
+    boolean match(String word, String pattern) {
+        int n = word.length();
+        if (pattern.length() != n) {
+            return false;
+        }
+        for (int i=0; i<n; i++) {
+            if (pattern.charAt(i) == '.') {
+                continue;
+            }
+            char ch = word.charAt(i);
+            if (ch != pattern.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean search(String word) {
+        for (String w : words) {
+            if (match(w, word)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
