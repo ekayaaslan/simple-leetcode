f3355dd6875cb30d57bb30f9d46faaaac2218669
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-07 13:04:42 +0000

    Create HouseRobber.java

---
+++ HouseRobber.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/house-robber
+// LC198. House Robber
+class HouseRobber {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) { return nums[0]; }
+        int[] money = new int[n];
+        money[n-1] = nums[n-1];
+        money[n-2] = Math.max(nums[n-2], nums[n-1]);
+        for (int i=n-3; i>=0; i--) {
+            money[i] = Math.max(money[i+1], nums[i]+money[i+2]);
+        }
+        return Math.max(money[0],money[1]);
+    }
+}
