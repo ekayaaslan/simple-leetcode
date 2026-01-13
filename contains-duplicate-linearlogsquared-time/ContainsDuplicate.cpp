c2889aa422fa82058b3c28b00a48663bf0e78db8
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-13 15:34:14 +0000

    Update ContainsDuplicate.cpp

--- contains-duplicate-linearlogsquared-time/ContainsDuplicate.cpp
+++ contains-duplicate-linearlogsquared-time/ContainsDuplicate.cpp
@@ -3,6 +3,18 @@
 // Concepts: Divide and conquer, Recursive search, Balanced trees
 class ContainsDuplicate {
 private:
+    bool search(vector<int>& nums, int lo, int mid, int hi) {
+        set<int> numset;
+        for (int i=lo; i<=mid; i++) {
+            numset.insert(nums[i]);
+        }
+        for (int i=mid+1; i<=hi; i++) {
+            if (numset.contains(nums[i])) {
+                return true;
+            }
+        }
+        return false;        
+    }
     bool search(vector<int>& nums, int lo, int hi) {
         if (lo == hi) {
             return false;
@@ -14,16 +26,7 @@ private:
         if (search(nums, mid+1, hi)) {
             return true;
         }
-        set<int> numset;
-        for (int i=lo; i<=mid; i++) {
-            numset.insert(nums[i]);
-        }
-        for (int i=mid+1; i<=hi; i++) {
-            if (numset.contains(nums[i])) {
-                return true;
-            }
-        }
-        return false;
+        return search(nums, lo, mid, hi);
     }
 public:
     bool containsDuplicate(vector<int>& nums) {
