380a0156f48410dbc4ea43805749bbfd0a86d8aa
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-13 13:49:57 +0000

    Create ContainsDuplicate.cpp

---
+++ contains-duplicate-linearithmicish-time/ContainsDuplicate.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/contains-duplicate
+// Linearithmicish time, linearish extra-space
+// Concepts: Divide and conquer, Recursive search, Hashing.
+class ContainsDuplicate {
+private:
+    bool search(vector<int>& nums, int lo, int hi) {
+        if (lo == hi) {
+            return false;
+        }
+        int mid = (lo+hi)/2; //lo-mid, mid+1-hi
+        if (search(nums, lo, mid)) {
+            return true;
+        }
+        if (search(nums, mid+1, hi)) {
+            return true;
+        }
+        unordered_set<int> numset;
+        for (int i=lo; i<=mid; i++) {
+            numset.insert(nums[i]);
+        }
+        for (int i=mid+1; i<=hi; i++) {
+            if (numset.contains(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        return search(nums, 0, n-1);
+    }
+};
