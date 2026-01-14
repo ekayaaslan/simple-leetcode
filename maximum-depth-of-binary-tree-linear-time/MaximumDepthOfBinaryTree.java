1856c0f63eb35a67675f3248dfa40287641937c1
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-14 22:06:42 +0000

    Drop java codes.

--- maximum-depth-of-binary-tree-linear-time/MaximumDepthOfBinaryTree.java
+++
@@ -1,9 +0,0 @@
-// https://leetcode.com/problems/maximum-depth-of-binary-tree
-// LC104. Maximum Depth of Binary Tree
-// Linear time, constant extra-space.
-class MaximumDepthOfBinaryTree {
-    public int maxDepth(TreeNode root) {
-        if (root == null) { return 0; }
-        return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
-    }
-}
