b0fe395ec111332bf7a396302001000b20078227
ekayaaslan <31211260+ekayaaslan@users.noreply.github.com>
2026-01-16 23:08:07 +0000

    Bucket sort solutions for topk frequent.

--- 05-top-k-frequent-elements/TopKFrequentElementsS6.cpp
+++ 05-top-k-frequent-elements/TopKFrequentElementsS6.cpp
@@ -1,3 +1,29 @@
 // https://leetcode.com/problems/top-k-frequent-elements
-// Linear time, 
+// Linear time, linear-ish extra-space.
 // Concepts: Hashing, Counting, Bucket sort.
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> counts;
+        for (int num : nums) {
+            counts[num] ++;
+        }
+        int n = nums.size();
+        vector<vector<int>> buckets(n+1);
+        for (auto& [num, count] : counts) {
+            buckets[count].push_back(num);
+        }
+        vector<int> out;
+        int i = n;
+        while (k > 0) {
+            if (buckets[i].empty()) {
+                i --;
+                continue;
+            }
+            out.push_back(buckets[i].back());
+            buckets[i].pop_back();
+            k --;
+        }
+        return out;
+    }
+};
--- 05-top-k-frequent-elements/TopKFrequentElementsS7.cpp
+++ 05-top-k-frequent-elements/TopKFrequentElementsS7.cpp
@@ -1,3 +1,31 @@
 // https://leetcode.com/problems/top-k-frequent-elements
-// Linear time, 
+// Linear time, linear-ish extra-space.
 // Concepts: Hashing, Running counts, Bucket sort.
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> counts;
+        int n = nums.size();
+        vector<vector<int>> buckets(n+1);
+        for (int num : nums) {
+            int c = ++counts[num];
+            buckets[c].push_back(num);
+        }
+        vector<int> out;
+        int i = n;
+        while (k > 0) {
+            if (buckets[i].empty()) {
+                i --;
+                continue;
+            }
+            int num = buckets[i].back();
+            buckets[i].pop_back();
+            if (counts.contains(num)) {
+                counts.erase(num);
+                out.push_back(num);
+                k --;
+            }
+        }
+        return out;
+    }
+};
